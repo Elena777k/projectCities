f3f635e5b303b8f10946cd26ddb5b0424b10df69
Elena777k <127039192+Elena777k@users.noreply.github.com>
2023-07-10 13:56:11 +0300

    5

---
+++ src/main/java/org/example/ModalColors.java
@@ -0,0 +1,22 @@
+package src.main.java.org.example;
+
+import java.awt.*;
+
+public enum ModalColors {
+    BLUE(new Color(0, 191, 255)),
+    LIGHT_YELLOW(new Color(253, 255, 150)),
+    PURPLE(new Color(154, 17, 217)),
+    LIGHT_BLUE(new Color(204, 252, 255)),
+    DARK_BLUE(new Color(21, 7, 46));
+
+    private final Color color;
+
+    ModalColors(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
